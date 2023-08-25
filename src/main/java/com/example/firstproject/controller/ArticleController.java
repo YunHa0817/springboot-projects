1d2e773d4e2af6a7c8c49d75f9bdbbac958df612
YunHa <yhhwang0283@gmail.com>
2023-08-25 15:38:58 +0900

    feat: CRUD를 이용한 게시판 만들기_third commit - 게시글 읽기_Read 추가 - 단일 데이터 조회 기능 - 데이터 목록 조회 기능

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -6,8 +6,12 @@ import com.example.firstproject.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
 
 @Slf4j  //로깅 기능을 위한 어노테이션 추가
 @Controller
@@ -36,5 +40,31 @@ public class ArticleController {
         log.info(saved.toString());
         //System.out.println(saved.toString());   //article이 DB에 잘 저장되는지 확인 출력
         return "";
+    }
+
+    @GetMapping("/articles/{id}")   //데이터 조회 요청 접수
+    public String show(@PathVariable Long id, Model model) { //매개변수로 id 받아 오기
+        log.info("id = " + id); //id를 받았는지 확인하는 로그 찍기
+
+        //1. id를 조회해 데이터 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+
+        //2. 모델에 데이터 등록하기
+        model.addAttribute("article", articleEntity);
+
+        //3. 뷰 페이지 반환하기
+        return "articles/show";
+    }
+
+    @GetMapping("/articles")
+    public String index(Model model) {
+        //1. 모든 데이터 가져오기
+        ArrayList<Article> articleEntityList = articleRepository.findAll();
+
+        //2. 모델에 데이터 등록하기
+        model.addAttribute("articleList", articleEntityList);
+
+        //3. 뷰 페이지 설정하기
+        return "articles/index";
     }
 }
--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -5,11 +5,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-@AllArgsConstructor //Article() 생성자를 대체하는 어노테이션 추가
-@ToString           //toString() 메서드를 대체하는 어노테이션 추가
-@Entity             //엔티티 선언
+@AllArgsConstructor     //Article(Long id, String title, String content) 생성자를 대체하는 어노테이션 추가
+@NoArgsConstructor      //Article() 생성자를 대체하는 어노테이션 추가
+@ToString               //toString() 메서드를 대체하는 어노테이션 추가
+@Entity                 //엔티티 선언
 public class Article {
     @Id //엔티티의 대푯값 지정
     @GeneratedValue //자동 생성 기능 추가(숫자가 자동으로 매겨짐)
@@ -19,22 +21,4 @@ public class Article {
     private String title;
     @Column //DB 테이블의 content열과 연결
     private String content;
-
-    /*
-    //Article 생성자 추가
-    public Article(Long id, String title, String content) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-    }
-
-    @Override
-    public String toString() {
-        return "Article{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
-     */
 }
