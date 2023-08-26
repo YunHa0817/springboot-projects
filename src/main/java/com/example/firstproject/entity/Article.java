d14a9fd1261ac31f332d39e3740ca9a9c235dfe3
YunHa <yhhwang0283@gmail.com>
2023-08-26 15:09:29 +0900

    feat: CRUD를 이용한 게시판 만들기_seventh commit - JPA 로깅 설정 및 SQL 쿼리 로그 확인 기능 추가

--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -1,9 +1,6 @@
 package com.example.firstproject.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,7 +13,7 @@ import lombok.ToString;
 @Getter                 //getter 메서드 추가
 public class Article {
     @Id //엔티티의 대푯값 지정
-    @GeneratedValue //자동 생성 기능 추가(숫자가 자동으로 매겨짐)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //DB가 id 자동 생성
     private Long id;
 
     @Column //DB 테이블의 title열과 연결
