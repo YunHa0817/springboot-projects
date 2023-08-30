d09b134141bfb532cba237d9a192db69c29c57d2
YunHa <yhhwang0283@gmail.com>
2023-08-30 23:20:02 +0900

    feat: CRUD를 이용한 게시판 만들기_fifteenth commit -댓글 CRUD 기능 추가     -댓글 수정하기

--- src/main/java/com/example/firstproject/api/CommentApiController.java
+++ src/main/java/com/example/firstproject/api/CommentApiController.java
@@ -9,49 +9,45 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController //REST 컨트롤러 선언
+@RestController
 public class CommentApiController {
     @Autowired
-    private CommentService commentService;  //댓글 서비스 객체 주입
-
-    //1. 댓글 조회
-    @GetMapping("/api/articles/{articleId}/comments")   //댓글 조회 요청 접수
+    private CommentService commentService;
+    // 1. 댓글 조회
+    @GetMapping("/api/articles/{articleId}/comments")
     public ResponseEntity<List<CommentDto>> comments(@PathVariable Long articleId) {
-        //서비스에 위임
+        // 서비스에 위임
         List<CommentDto> dtos = commentService.comments(articleId);
-
-        //결과 응답
+        // 결과 응답
         return ResponseEntity.status(HttpStatus.OK).body(dtos);
     }
 
-    //2. 댓글 생성
-    @PostMapping("/api/articles/{articleId}/comments")  //댓글 생성 요청 접수
+    // 2. 댓글 생성
+    @PostMapping("/api/articles/{articleId}/comments")
     public ResponseEntity<CommentDto> create(@PathVariable Long articleId,
                                              @RequestBody CommentDto dto) {
-        //서비스에 위임
+        // 서비스에 위임
         CommentDto createdDto = commentService.create(articleId, dto);
-
-        //결과 응답
+        // 결과 응답
         return ResponseEntity.status(HttpStatus.OK).body(createdDto);
     }
 
-    //3. 댓글 수정
-    @PatchMapping("api/comments/{id}")  //댓글 수정 요청
-    public ResponseEntity<CommentDto> update(@PathVariable Long id, @RequestBody CommentDto dto) {
-        //서비스에 위임
+    // 3. 댓글 수정
+    @PatchMapping("/api/comments/{id}")
+    public ResponseEntity<CommentDto> update(@PathVariable Long id,
+                                             @RequestBody CommentDto dto) {
+        // 서비스에 위임
         CommentDto updatedDto = commentService.update(id, dto);
-
-        //결과 응답
+        // 결과 응답
         return ResponseEntity.status(HttpStatus.OK).body(updatedDto);
     }
 
-    //4. 댓글 삭제
-    @DeleteMapping("/api/comments/{id}")    //댓글 삭제 요청 접수
+    // 4. 댓글 삭제
+    @DeleteMapping("/api/comments/{id}")
     public ResponseEntity<CommentDto> delete(@PathVariable Long id) {
-        //서비스에 위임
+        // 서비스에 위임
         CommentDto deletedDto = commentService.delete(id);
-
-        //결과 응답
+        // 결과 응답
         return ResponseEntity.status(HttpStatus.OK).body(deletedDto);
     }
 }
